829eac6c50ab0bcb558a9fed63b66a1af979ccdb
Zach Bjornson <zbbjornson@gmail.com>
2021-01-03 15:42:57 -0800

    set runpath for copied SOs

--- prebuild/Linux/bundle.sh
+++ prebuild/Linux/bundle.sh
@@ -2,8 +2,9 @@ set -ex
 
 copies=$(lddtree.sh -l build/Release/canvas.node | sed -r -e '/^\/lib/d' -e '/canvas.node$/d');
 
-echo $copies
+apt install -y patchelf
 
 for so in $copies; do
   cp $so build/Release
+  patchelf --set-rpath '$ORIGIN' build/Release/$(basename $so)
 done;
